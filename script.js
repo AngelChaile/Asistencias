0078dbf513712f7b6fb216a90eea7f6b8812e8bd
Angel Alejandro <angelchaile90@gmail.com>
2025-10-08 09:09:47 -0300

    new implement

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // === CONFIGURACIÓN ===
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwVKbPIL-lBS-NjVUS3IUHyWLW9bN443AtTsTkkh8iCrFglY14TAsAPXkTNZBBuPHgD/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz5krucO0DTcQeMXsoDYyD4cjeP6N1PO8lHeDqeqD8AqhYNuFu5hiIveh8hhozYvajV/exec";
 
 // === ELEMENTOS DEL DOM ===
 const btnGenerarQR = document.getElementById("btnGenerarQR");
